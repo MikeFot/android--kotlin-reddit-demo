979304e0d4759417fb619b459a26a6c3e202737e
Fotiadis <michaelfotiadis@gmail.com>
2021-09-09 13:30:07 +0100

    Initial project commit (gradle kts, view binding, empty activity, application icon)

---
+++ settings.gradle.kts
@@ -0,0 +1,22 @@
+pluginManagement {
+    @Suppress("UnstableApiUsage")
+    plugins {
+        id("org.jetbrains.kotlin.jvm") version "1.5.21"
+    }
+    repositories {
+        google()
+        mavenCentral()
+        maven(url = "https://plugins.gradle.org/m2/")
+    }
+}
+
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "Android Reddit Demo"
+include(":app")
